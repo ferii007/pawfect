e64b56ac8b05a627e3429b098e7f37822155fc75
Feri Ramdani (personal) <ferii.ramdani007@gmail.com>
2024-09-05 20:21:19 +0700

    Update Hero Section - update responsive

--- src/app/components/hero_section/HeroSection.jsx
+++ src/app/components/hero_section/HeroSection.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const HeroSection = () => {
     return (
-        <section>
+        <section className="relative">
             <div className="absolute left-0 top-0 -z-20">
                 <svg width="892" height="794" viewBox="0 0 892 794" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <defs>
@@ -18,24 +18,24 @@ const HeroSection = () => {
                 </svg> 
             </div>
 
-            <div className="absolute right-0 -bottom-5 -z-10">
+            <div className="select-none absolute -bottom-full lg:-bottom-32 right-0 -z-10">
                 <Image
-                    className="w-[45rem] h-[45rem]"
+                    className="w-full h-full md:w-[40rem] md:h-[40rem] lg:w-[30rem] lg:h-[30rem] xl:w-[38rem] xl:h-[38rem]"
                     src={heroImg}
-                    alt="Hero Image - pet child"
-                    priority
+                    alt="Pawfect Img - Gambar hewan peliharaan sedang mandi spa dengan ekspresi bahagia"
+                    // priority
                 />
             </div>
 
-            <div className="pl-12 pt-12 flex flex-col gap-10 justify-start justify-items-start">
+            <div className="responsive-padding-l pt-6 md:pt-8 lg:pt-12 flex flex-col gap-5 md:gap-8 lg:gap-10">
                 <div className="max-w-fit">
-                    <a href="" className="flex items-center">
+                    <a href="" className="flex items-center select-none">
                         <Image
                             src={logoImg}
                             alt="Pawfect logo"
                             width={80}
                             height={80}
-                            priority
+                            // priority
                         />
 
                         <h1 className="text-title text-xl font-bold flex flex-col leading-none">
@@ -45,11 +45,11 @@ const HeroSection = () => {
                     </a>
                 </div>
 
-                <h2 className="text-title text-7xl max-w-4xl">
+                <h2 className="text-title">
                     Grooming Asik buat Sahabat Berbulumu
                 </h2>
 
-                <p className="text-desc text-sm max-w-96 leading-relaxed">
+                <p className="text-desc">
                     Bikin hewan peliharaanmu makin sehat dan terawat dengan layanan grooming terbaik dari kami. Mulai dari potong bulu, mandi spa, sampai perawatan lengkap biar mereka tetap sehat dan nyaman. Serahkan ke kami, dan lihat sendiri hasilnya!
                 </p>
 
